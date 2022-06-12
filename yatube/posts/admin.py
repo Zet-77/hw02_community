b5557d0c0196ad89ca225ad17250a373a11bff65
Azimov Zaur <aminamumina2013@gmail.com>
2022-06-12 04:08:37 +0400

    second

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
 from .models import Post
 from .models import Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # перечисляем поля, которые должны отображаться в админке.
+
     list_display = (
         'pk',
         'text',
@@ -14,14 +14,9 @@ class PostAdmin(admin.ModelAdmin):
         'author',
         'group',
     )
-    # Это позволит изменять поле "group" в любом посте без
-    # лишних движений мышкой, прямо из списка постов.
     list_editable = ('group',)
-    # Добовляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате.
     list_filter = ('pub_date',)
-    # На случай пустого поля
     empty_value_display = '-пусто-'
 
 
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,28 +1,57 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-# Create your models here.
+
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField('Информация о группах', max_length=200)
-    slug = models.SlugField(max_length=200, unique=True, db_index=True)
-    description = models.TextField()
+    title = models.CharField(
+        verbose_name='Информация о группах',
+        max_length=200
+    )
+    slug = models.SlugField(
+        max_length=200, unique=True,
+        db_index=True, verbose_name='slug'
+    )
+    description = models.TextField(
+    verbose_name='Описание'
+    )
+    related_name=('group')
+
+    class Meta:
+        verbose_name_plural = 'groups'
 
     def __str__(self):
-        return self.title
+        return self.title  
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        max_length=30, verbose_name='Текст'
+    )
+    pub_date = models.DateTimeField(
+    auto_now_add=True, verbose_name='Дата'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name=('posts'),
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group, blank=True, null=True,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Группа'
     )
+
+    def __str__(self):
+        return self.text 
+
+    
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name_plural='posts'
+
+    
