5b09c1c0d18f28af03692cb2e50f6e5f69c9b5be
Azimov Zaur <aminamumina2013@gmail.com>
2022-07-02 16:13:11 +0400

    4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,13 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404, render
+
 from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:settings.LIMIT]
-    title = 'Главная страница'
+    posts = Post.objects.all()
     context = {
         'posts': posts,
-        'title': title,
     }
     return render(request, 'posts/index.html', context)
 
