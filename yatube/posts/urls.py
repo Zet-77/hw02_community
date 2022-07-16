4088131ccf7e1e6ab89d8edb1102d36273b4b37c
Azimov Zaur <aminamumina2013@gmail.com>
2022-07-16 23:07:11 +0300

    !

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,4 +7,12 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('password_change/', views.PasswordChandeView.as_view(), name='password_change'),
+    path('password_change/Done', views.PasswordChangeDoneViews.as_view(), name='password_change_done'),
+    path('password_reset', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompieteView.as_view(), name='password_reset_complete'),
 ]
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,6 +3,8 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
     path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('users.urls')),*
+    path('auth/', include('django.contrib.auth.urls')),
 ]
